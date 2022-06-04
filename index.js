06c29f584b4a3044c7b5d0227bd3ebfd815314c3
Deenaz Fathima <63476604+fathimadeenaz@users.noreply.github.com>
2022-06-04 23:17:26 +0530

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+const PORT = process.env.PORT || 8000
+const axios = require("axios").default
+const express = require("express")
+const cors = require("cors")
+require('dotenv').config()
+const app = express()
+
+app.use(cors())
+
+app.get('/word', (req, res) => {
+    const options = {
+        method: 'GET',
+        url: 'https://random-words5.p.rapidapi.com/getRandom',
+        params: {wordLength: '5'},
+        headers: {
+            'X-RapidAPI-Host': 'random-words5.p.rapidapi.com',
+            'X-RapidAPI-Key': process.env.RAPID_API_KEY
+        }
+    }
+    
+    axios.request(options).then(response => {
+        console.log(response.data)
+        res.json(response.data)
+    }).catch(error=> {
+        console.error(error)
+    })
+})
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
